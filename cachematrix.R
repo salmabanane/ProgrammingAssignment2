bc4fae45194763dfe290a3cecb6d4329cfa95edf
salmabanane <114592018+salmabanane@users.noreply.github.com>
2022-09-28 13:52:30 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program will help us to create an environment were we will be able to cache the inverse of a matrix 
+## by using two levels.
+## first level control the operation made (inverse the matrix) and the second one who carry out the work 
 
-## Write a short comment describing this function
+## makeCacheMatrix is the first level of the environment where we re going to create th matrix and cache the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+}
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve is the second level who calculate the inverse of the matrix that has been manipulated in the first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
